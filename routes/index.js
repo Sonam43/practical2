94704597c9a44eccc4d4bc1cdad1d1f7cf2cf2ef
Sonam43 <07230043.sherubtse@rub.edu.bt>
2025-03-20 19:15:08 +0600

    Delete routes directory

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-// Render the landing page
-router.get('/', (req, res) => {
-res.render('index', { title: 'Online Book Store' });
-});
-module.exports = router;
